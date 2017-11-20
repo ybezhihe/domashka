ac7e43ef16f73ab087fffc0919104cd86716e8bd
ybezhihe <31882125+ybezhihe@users.noreply.github.com>
2017-11-20 12:48:49 +0300

    новая версия дз

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,5 +1,5 @@
-with open("text.txt") as f:
-    text = f.read().decode('utf-8').split()
+with open("C:\Users\пп\Desktop\прога\питон\hw5\txt.txt",encoding="utf-8") as f:
+    text = f.read().split()
     x = Counter()
     for s in f:
        x[s] += 1
