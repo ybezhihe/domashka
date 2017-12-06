9e9bb949d16e1ec1aadcf04924d1b5a117fd79e5
ybezhihe <31882125+ybezhihe@users.noreply.github.com>
2017-12-07 05:13:42 +1100

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -2,7 +2,7 @@ y = str(input())
 x = 2
 with open("C:/Users/пп/Desktop/homework.txt", "w", encoding = "utf-8") as f:
     while x > 1:
-        f.write(y, \n)
+        f.write(y)
         #f.write("\n".join(list).join("\n"))
         x = x + 1
         if f = pass:
