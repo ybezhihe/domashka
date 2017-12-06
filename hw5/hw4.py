0ed473fb497bb6252e5d1ae25f21220486a06aa3
ybezhihe <31882125+ybezhihe@users.noreply.github.com>
2017-12-07 04:17:51 +1100

    здесь дз №4!!!!

--- hw5/hw4.py
+++ hw5/hw4.py
@@ -1,4 +1,4 @@
-with open("C:\Users\пп\Desktop\прога\питон\hw5\txt.txt",encoding="utf-8") as f:
+with open("C:\Users\пп\Desktop\прога\питон\hw5\txt.txt",encoding="utf-8") as f :
     text = f.read().split()
     x = Counter()
     for s in f:
