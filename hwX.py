9e2e8b9c492368de144f6b3cb1692455cfaf9c28
ybezhihe <31882125+ybezhihe@users.noreply.github.com>
2017-12-06 21:12:42 +0300

    \hw5

---
+++ hwX.py
@@ -0,0 +1,11 @@
+y = str(input())
+x = 2
+with open("C:/Users/пп/Desktop/homework.txt", "w", encoding = "utf-8") as f:
+    while x > 1:
+        f.write(y, \n)
+        #f.write("\n".join(list).join("\n"))
+        x = x + 1
+        if f = pass:
+            break
+f.close()
+        
